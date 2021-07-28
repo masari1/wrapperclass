85a82d413a341ae316e33acc3d76620ea52c6c3c
Malik Sari <Malik.Sari@outlook.com>
2021-07-28 14:51:31 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+
+    public static void main (String [] args){
+
+
+
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 3.14;
+        String e = "Bro";
+
+        
+    }
+
+
+
+
+
+
+}
